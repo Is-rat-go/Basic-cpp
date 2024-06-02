98883043f2292dd535371f6bb8a7c0eb975d5fc1
Israt Jahan <156769904+Is-rat-go@users.noreply.github.com>
2024-06-02 13:04:53 +0600

    Add files via upload
    basic c++ it good same as python basic

---
+++ first.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+//this for input print:
+/*int main(){
+
+cout<<"Assalamu Alikum C++";
+return 90;
+
+}
+*/
+
+
+// this for get input from user:
+
+/*int main(){
+char name[20];
+cout<<"write here:"; //user ekhane likbe
+cin>>name; //user jeta likbe oita er modde store hobe
+cout<<"Welcome "<<name<< '!'; // jeta store hoisilo oita ekhan theke print hoise ta show korbe with welcome er sathe!
+return 90;
+}
+*/
+
+
+/*ALHAMDULLILAH...I...CAN PLZ DONOT BE STRESSED ABOUT RESULT...GO FOR YOUR PASSION AND GIVE THE BEST...I KNOW YOU CAN IN SHA ALLAH
+
+int main(){
+
+int num1,num2,sum;//ami jayga kore disi user ke
+cout<<"enter two number:\n";//tai user 2 ta number nite parse
+cin>>num1>>num2;// r user 2 ta number nite parse bolei oi 2 ta number ami store kore raksii cin diye!
+sum=num1+num2;//then sum vari te 2 ta num add kore store kore raklam
+cout<<"sum of entered two number is:"<<sum;//then sum vari k call kore cout diye print kore raklam.sesh!khatam!!!byee
+return 90;// then sucessfully run korle oy amare 90 pathaye santona dibe sesh khatam ho gayii
+}
+*/
+
+int main(){
+
+
+
+
+
+
+
+
+
+}
+
+
